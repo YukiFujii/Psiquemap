752b0b67058e9e54c286c459e3450492517b0d61
yuki <yuki.fujii@hotmail.com>
2016-11-28 14:54:32 -0200

    BD Paciente, Diário, QuestionarioMINI, Acontecimento, Sintomas, Sintomas Sentidos, Feedback, Parte da Medicação I a III, RespostaMINI, RespostasMINI, RespostaDiario, RespostasDiario,Parte da Medicação IV, Login(Controle), LoginWS I doing.

--- app/src/main/java/com/example/psiquemap/psiquemap/LoginActivity.java
+++ app/src/main/java/com/example/psiquemap/psiquemap/LoginActivity.java
@@ -11,10 +11,8 @@
     import android.widget.Button;
     import android.widget.EditText;
 
-    import com.example.psiquemap.psiquemap.comunicacao.HttpClient;
     import com.example.psiquemap.psiquemap.comunicacao.LoginWS;
     import com.example.psiquemap.psiquemap.entidades.Controle;
-    import com.example.psiquemap.psiquemap.entidades.Dados;
     import com.example.psiquemap.psiquemap.entidades.Login;
     import com.example.psiquemap.psiquemap.entidades.Medicamento;
     import com.example.psiquemap.psiquemap.entidades.Paciente;
@@ -28,8 +26,6 @@
     import com.example.psiquemap.psiquemap.sql.PerguntasDoQuestionarioMINI;
     import com.example.psiquemap.psiquemap.sql.Sintomas;
     import com.google.gson.Gson;
-
-    import java.io.InputStream;
 
  public class LoginActivity extends AppCompatActivity
  {
@@ -128,56 +124,13 @@
          return acesso;
      }
 
-     public static String POST(String url, String person){
-         InputStream inputStream = null;
-         String result = "";
-         try {
+     public void btnlogin(View view)
+     {
+         String restUrl = "http://192.168.0.15:8080/psiquemap-ws/login";
+         new LoginOperation().execute(restUrl);
 
-             // 1. create HttpClient
-             HttpClient httpclient = new DefaultHttpClient();
-
-             // 2. make POST request to the given URL
-             HttpPost httpPost = new HttpPost(url);
-
-             // 3. build jsonObject
- ;
-
-             // 4. convert JSONObject to JSON to String
-             json = jsonObject.toString();
-
-             // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-             // ObjectMapper mapper = new ObjectMapper();
-             // json = mapper.writeValueAsString(person);
-
-             // 5. set json to StringEntity
-             StringEntity se = new StringEntity(json);
-
-             // 6. set httpPost Entity
-             httpPost.setEntity(se);
-
-             // 7. Set some headers to inform server about the type of the content
-             httpPost.setHeader("Accept", "application/json");
-             httpPost.setHeader("Content-type", "application/json");
-
-             // 8. Execute POST request to the given URL
-             HttpResponse httpResponse = httpclient.execute(httpPost);
-
-             // 9. receive response as inputStream
-             inputStream = httpResponse.getEntity().getContent();
-
-             // 10. convert inputstream to string
-             if(inputStream != null)
-                 result = convertInputStreamToString(inputStream);
-             else
-                 result = "Did not work!";
-
-         } catch (Exception e) {
-             Log.d("InputStream", e.getLocalizedMessage());
-         }
-
-         // 11. return result
-         return result;
      }
+
 
      @SuppressLint("NewApi")
      private void chamarLoginWS(final String dataJson)
---
+++ app/src/main/java/com/example/psiquemap/psiquemap/comunicacao/LoginOperation.java
@@ -0,0 +1,28 @@
+
+
+import android.os.AsyncTask;
+import org.apache.http.client.HttpClient;
+
+/**
+ * Created by yuki on 28/11/16.
+ */
+
+public class LoginOperation extends AsyncTask<String, Void, Void>
+{
+    final HttpClient httpClient =
+
+    protected void onPreExecute()
+    {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(String... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+    }
+}
